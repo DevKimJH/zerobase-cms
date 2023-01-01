41937094e4034cd077e9f08f922b3747c2bdcaee
DevKimJH <hg116710@naver.com>
2023-01-01 17:29:11 +0900

    feat : [007-scratch-user] test용 스크래치 파일 및 api 컨벤션 수정

--- src/main/java/com/zerobase/cms/user/config/filter/CustomerFilter.java
+++ src/main/java/com/zerobase/cms/user/config/filter/CustomerFilter.java
@@ -17,20 +17,17 @@ public class CustomerFilter implements Filter {
     private final JwtAuthenticationProvider jwtAuthenticationProvider;
     private final CustomerService customerService;
 
-
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         String token = req.getHeader("X-AUTH-TOKEN");
-
         if(!jwtAuthenticationProvider.validateToken(token)){
             throw new ServletException("Invalid Access");
         }
-
         UserVo vo = jwtAuthenticationProvider.getUserVo(token);
-        customerService.findByIdAndEmail(vo.getId(), vo.getEmail()).orElseThrow(
-                () -> new ServletException("Invalid access")
-        );
+        customerService.findByIdAndEmail(vo.getId(), vo.getEmail())
+                .orElseThrow(() -> new ServletException("Invalid Access"));
 
         chain.doFilter(request, response);
     }
